ddd58691a70da147025e3fb2c3d201a912d65b88
Stefanie David <127105234+stefaniegdavid@users.noreply.github.com>
2023-06-08 14:53:46 -0700

    Add files via upload

---
+++ React/first_jsx/my-app/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello Dojo!</h1>
+      <h2>Things I need to do:</h2>
+      <p>*Learn React</p>
+      <p>*Climb Mt. Everest</p>
+      <p>*Run a marathon</p>
+      <p>*Feed the dogs</p>
+    </div>
+  );
+}
+
+export default App;
